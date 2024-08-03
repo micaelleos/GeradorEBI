f9ca2a82225467a53c1b305e50033d7a5b9ace34
micaelleos <micaelle.oliveira10@hotmail.com>
2024-08-03 17:36:28 -0300

    centered exibition

--- main.py
+++ main.py
@@ -5,22 +5,6 @@ from chathandeler import ChatService
 from sidebar import sidebar_list
 from styles import *
 
-def atualizar_estudo(ebi):
-    print(ebi)
-    if ebi:
-        st.session_state.title = ebi["title"]
-        st.session_state.base = ebi["base"]
-        st.session_state.description = ebi["description"]
-        st.session_state.footer = ebi["footer"]
-
-if "title" not in st.session_state: 
-    st.session_state.title = ""
-    st.session_state.base = ""
-    st.session_state.description = ""
-    st.session_state.footer = ""
-    
-
-
 styles()
 chat = ChatService()
 
@@ -35,40 +19,49 @@ if "messages" not in st.session_state:
     st.session_state.messages = chat.history(st.session_state.chat_id)
 
 with st.container():
+    
+    with st.container():
+        col1, col2 = st.columns([0.8,0.2])
+        with col1:
+            st.title("Assistente de EBI")
+        with col2:
+            if st.button(":gear:",use_container_width=True):
+                pass
+    
     col11, col22 = st.columns([0.6,0.4])
     with col11:
-        with st.container():
-            with st.container():
-                col1, col2 = st.columns([0.8,0.2])
-                with col1:
-                    st.title("Assistente de EBI")
-                with col2:
-                    if st.button(":gear:",use_container_width=True):
-                        pass
-
-            for message in st.session_state.messages:
-                if message['role'] == "assistant":
-                    with st.chat_message(message["role"]):
-                        st.markdown(message["content"])
-                else:
-                    with st.chat_message(message["role"]): 
-                        st.markdown(message["content"])
+        with st.container(border=False):
+            chat_container = st.container(height=400,border=False)
+            with chat_container:
+                initial_message = st.chat_message("assistant")
+                initial_message.write("Olá, como posso ajudá-lo hoje?")  
                 
+                messages = st.session_state.messages
 
-            if prompt := st.chat_input("Faça uma pergunta?"):
+                for i in range(0,len(messages)):
+                    message = messages[i]       
+                                     
+                    if message['role'] == "assistant":
+                        with st.chat_message(message["role"]):
+                            st.markdown(message["content"])
+                    else:
+                        with st.chat_message(message["role"]):
+                            st.markdown(message["content"])
+                
+            
+            prompt = st.chat_input("Faça uma pergunta?",key="user_input")
+
+            if prompt:
+                
                 st.session_state.messages.append({"role": "user", "content": prompt})
-                with st.chat_message("user"):
-                    st.markdown(prompt)
-
-                with st.chat_message("assistant"):
-                    with st.spinner(''):
-                        ebi,response=chat.chat(prompt,st.session_state.chat_id,context)
-                        atualizar_estudo(ebi)
-                    st.write(response)
+                
+                ebi,response=chat.chat(prompt,st.session_state.chat_id,context)
+                
                 st.session_state.messages.append({"role": "assistant", "content": response})
+
+                st.rerun()
+
     with col22:
-        with st.container(border=True,height=1000):
-            st.write(st.session_state.title)
-            st.write(st.session_state.base)
-            st.write(st.session_state.description)
-            st.write(st.session_state.footer)
+        with st.container(border=True):
+            st.write('Estudo biblico')
+            #st.write(ebi)
--- styles.py
+++ styles.py
@@ -7,6 +7,23 @@ def styles():
                 { 
                 max-width: 90vw;
                 } 
+                
+                .st-emotion-cache-12fmjuu{
+                height: 1.75rem;
+                }
+
+                .st-emotion-cache-13ln4jf{
+                padding: 3rem 1rem 10rem;
+                }
+
+                .st-emotion-cache-bm2z3a{
+                overflow: hidden;
+                }
+
+                .st-emotion-cache-4uzi61{
+                height: 35vw;
+                overflow: auto;
+                }
                 </style>
                 """
                 , unsafe_allow_html=True)
